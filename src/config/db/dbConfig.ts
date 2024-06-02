41e9a34b45f1d268bdb6ab2ddc5ef861ca129236
Ayman <aymanuddin1111.au@gmail.com>
2024-06-02 15:35:57 +0530

    Conert password to string

--- src/config/db/dbConfig.ts
+++ src/config/db/dbConfig.ts
@@ -21,7 +21,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
+  password: String(process.env.DB_PASSWORD),
   port: Number(process.env.DB_PORT),
 };
 
